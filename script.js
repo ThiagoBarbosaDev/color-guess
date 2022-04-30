5a792023689fb96275b36cd03f9ca3d942254ce2
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-04-30 13:06:29 -0300

    requisito 6

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const subTitle = document.querySelector('#rgb-color');
 const ballsArray = document.querySelectorAll('.ball');
 const resultScreen = document.querySelector('#result-screen');
 const resultText = document.querySelector('#answer');
-const resetButton = document.querySelector('#reset-game')
+const resetButton = document.querySelector('#reset-game');
 
 function generateRGB() {
   const r = Math.floor(Math.random() * 255 + 1);
@@ -11,20 +11,22 @@ function generateRGB() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-const winnerRgb = generateRGB();
-
 function setInnerTextToSubtitle() {
-  subTitle.innerText = winnerRgb;
+  subTitle.innerText = generateRGB();
 }
+
+// let winnerRgb = subTitle.innerText;
 
 setInnerTextToSubtitle();
 
 function paintBall() {
+
   const winnerIndex = Math.floor(Math.random() * 6 + 1);
-  console.log(winnerIndex);
   for (let i = 0; i < ballsArray.length; i += 1) {
+    ballsArray[i].id = '';
     if (i === winnerIndex - 1) {
-      ballsArray[i].style.backgroundColor = winnerRgb;
+      
+      ballsArray[i].style.backgroundColor = subTitle.innerText;
       ballsArray[i].id = 'winner';
     } else {
       ballsArray[i].style.backgroundColor = generateRGB();
@@ -46,12 +48,14 @@ function winStateHandler() {
   });
 }
 
-winStateHandler()
+winStateHandler();
 
 function resetTheGame() {
   resetButton.addEventListener('click', () => {
-    resultText.innerHTML === 'Escolha uma cor'
-  })
+    resultText.innerHTML = 'Escolha uma cor';
+    subTitle.innerText = generateRGB();
+    paintBall();
+  });
 }
 
-resetTheGame()
+resetTheGame();
